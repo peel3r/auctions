3eab3df02f40ae949be08d6d9a8d27ba3a3f121f
peel3r <p@weljakubowski.com>
2015-05-18 14:41:38 +0100

    add auction feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518123603) do
+ActiveRecord::Schema.define(version: 20150518131011) do
+
+  create_table "auctions", force: :cascade do |t|
+    t.float    "value"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "auctions", ["product_id"], name: "index_auctions_on_product_id"
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
